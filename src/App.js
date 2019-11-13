f163f0a3597e8a63a8ae7f2a9f9b9cec9f1d15f3
Jaime Duque <jaimeduque17@gmail.com>
2019-11-12 20:56:32 -0500

    Initial structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app container">
+      <div className="jumbotron">
+        <p className="lead text-center">Image Finder</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
